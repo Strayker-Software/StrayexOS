90055ac4e7e26207e8d1afa4f54969592cc0dcca
StraykerPL <45916090+StraykerPL@users.noreply.github.com>
2019-03-05 10:15:10 +0100

    GPLv3 to MIT/X11

--- kernel/enter.asm
+++ kernel/enter.asm
@@ -2,8 +2,6 @@
 ; v1.0
 ; Kernel's entery point in NASM Assembly
 
-; TODO: Dodać nagłówek GPL!
-
 [BITS 32]			; Kernel loaded by GNU GRUB with Multiboot, working in 32 bit protected mode,
 
 GLOBAL _start			; Here will jump GRUB after loading,
