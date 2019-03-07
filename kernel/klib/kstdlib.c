10b19332a327d1ecf028b29896f4a7be9b013552
StraykerPL <45916090+StraykerPL@users.noreply.github.com>
2019-03-07 12:04:34 +0100

    kstdlib.c: Added itoa() implementation
    itoa(), swap() and reverse()

--- kernel/klib/kstdlib.c
+++ kernel/klib/kstdlib.c
@@ -4,6 +4,10 @@
  Kernel Standard Library file, access through "kstdlib.h"
  Copyright 2019 Daniel Strayker Nowak
  All rights reserved
+ 
+ Code added to file from:
+ https://www.geeksforgeeks.org/implement-itoa/
+ https://stackoverflow.com/questions/3835922/simple-swap-function-why-doesnt-this-one-swap
 */
 
 #include "kstdlib.h"
@@ -20,4 +24,67 @@ unsigned char kinportb (unsigned short _port)
 void koutportb (unsigned short _port, unsigned char _data)
 {
     __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
+}
+
+// Swapping integers in memory:
+void swap(int* a, int* b)
+{
+ int temp = *a;
+ *a = *b;
+ *b = temp;
+}
+
+// Reversing strings:
+void reverse(char str[], int length) 
+{
+    int start = 0; 
+    int end = length -1; 
+    while (start < end) 
+    { 
+        swap(*(str+start), *(str+end)); 
+        start++; 
+        end--; 
+    } 
+}
+
+// Converting integer to char table:
+char *itoa(int num, char* str, int base) 
+{ 
+    int i = 0; 
+    bool isNegative = false; 
+  
+    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    if (num == 0) 
+    { 
+        str[i++] = '0'; 
+        str[i] = '\0'; 
+        return str; 
+    } 
+  
+    // In standard itoa(), negative numbers are handled only with  
+    // base 10. Otherwise numbers are considered unsigned. 
+    if (num < 0 && base == 10) 
+    { 
+        isNegative = true; 
+        num = -num; 
+    } 
+  
+    // Process individual digits 
+    while (num != 0) 
+    { 
+        int rem = num % base; 
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0'; 
+        num = num/base; 
+    } 
+  
+    // If number is negative, append '-' 
+    if (isNegative) 
+        str[i++] = '-'; 
+  
+    str[i] = '\0'; // Append string terminator 
+  
+    // Reverse the string 
+    reverse(str, i); 
+  
+    return str; 
 }
