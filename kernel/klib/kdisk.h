6b206512efeb718f5cfeb447561a61068251ae92
StraykerPL <strayker@op.pl>
2019-12-04 22:18:19 +0100

    Kernel: started working over ATA DMA driver

---
+++ kernel/klib/kdisk.h
@@ -0,0 +1,14 @@
+/*
+ Strayex Kernel C Library "klib"
+ v1.1.0
+ Kernel ATA DMA header file, functions in "ata.c"
+ Copyright 2019 Daniel Strayker Nowak
+ All rights reserved
+*/
+
+#ifndef _KATA_
+#define _KATA_
+
+#include <stdbool.h>
+
+#endif
